4051218918b829e15d4270fbd4cb3688a603699e
nathanbud <nthnlbdky916@gmail.com>
2019-11-14 02:07:49 +0800

    added search form

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ app.set('view engine', 'handlebars');
 
 //Routes
 app.get('/', (req, res) =>{
-
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    res.render('hello-gif', {gifUrl});
+    console.log(req.query)
+    res.render('home');
+   
+   // const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    
     //res.send('Hello World');
 });
 
